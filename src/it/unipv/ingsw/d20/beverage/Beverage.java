707e5e2a5f0aef05cb10e0168c652dbbadcecfb3
fasu <fasulo.nicol@gmail.com>
2020-05-11 19:02:22 +0200

    minor changes

--- src/it/unipv/ingsw/d20/beverage/Beverage.java
+++ src/it/unipv/ingsw/d20/beverage/Beverage.java
@@ -8,6 +8,8 @@ public class Beverage {
 	
 	public Beverage(BeverageDescription b) throws InsufficientIngredientsException {
 		this.beverageDescription=b;
+		//INVECE DEL METODO hasBeenDelivered ABBIAMO PREFERITO USARE UN'ECCEZIONE CHE VIENE GESTITA NELLA SALE (IN CASO NON FOSSE POSSIBILE EROGARE LA BEVANDA. 
+		//L'ECCEZIONE DEVE ESSERE LANCIATA DAL COSTRUTTORE
 	}
 	
 	public void makeNewBeverage(BeverageDescription b) {
