c7c342fea69e6c319c4b2cb0317c5066c6cbb86a
Nicolò Fasulo <fasulo.nicol@gmail.com>
2020-05-11 16:25:19 +0200

    corrected Sale

--- src/it/unipv/ingsw/d20/paymentsystem/Sale.java
+++ src/it/unipv/ingsw/d20/paymentsystem/Sale.java
@@ -11,29 +11,34 @@ import it.unipv.ingsw.d20.beverage.BeverageDescription;
  * @author Nicolò Fasulo <fasulo.nicol@gmail.com>
  *
  */
+@SuppressWarnings("unused")
 public class Sale {
 	
 	private Date date;
-	private BeverageDescription beverageDescription;
 	private double amount;
-	private boolean isComplete = false;
+	private double price;
+	
+	private double change;
+	private boolean completed = false;
 	
 	public Sale(BeverageDescription beverageDescription, double amount) {
-		this.beverageDescription = beverageDescription;
 		this.amount = amount;
 		date = new Date();
+		price = beverageDescription.getPrice();
 		
-		Payment payment = new Payment(amount, beverageDescription.getPrice());
-		if (payment.isAccepted()) {
+		Payment payment = new Payment(amount, price);
+		
+		if (payment.isAccepted()) { //checks whether the payment was successful or not 
+			change = payment.getChange();
 			Beverage beverage = new Beverage(beverageDescription);
-			if (beverage.hasBeenDelivered()) {
-				isComplete = true;
+			if (beverage.hasBeenDelivered()) { //checks whether the beverage was correctly delivered or not
+				completed = true;
 			}
 		}
 	}
 
 	public boolean isComplete() {
-		return isComplete;
+		return completed;
 	}
 
 }
