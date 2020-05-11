735c01de46325958f3cd0eda2f5db15a5b0c3784
fasu <fasulo.nicol@gmail.com>
2020-05-11 14:43:23 +0200

    created paymentsystem package
    Payment and Sale classes added

---
+++ src/it/unipv/ingsw/d20/Tester.java
@@ -0,0 +1,15 @@
+package it.unipv.ingsw.d20;
+
+/**
+ * 
+ * For testing purposes only, leave this class empty!
+ *
+ */
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/it/unipv/ingsw/d20/paymentsystem/Sale.java
@@ -0,0 +1,11 @@
+package it.unipv.ingsw.d20.paymentsystem;
+
+/**
+ * 
+ * @author 
+ * @author Nicolò Fasulo <fasulo.nicol@gmail.com>
+ *
+ */
+public class Sale {
+
+}
