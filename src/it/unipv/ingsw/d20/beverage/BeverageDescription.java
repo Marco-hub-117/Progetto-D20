9afaa9a2e99134582367ce388510165753acce19
Enrico <luca.malovini01@universitadipavia.it>
2020-05-12 15:04:29 +0200

    Aggiunta BeverageDescription

--- src/it/unipv/ingsw/d20/beverage/BeverageDescription.java
+++ src/it/unipv/ingsw/d20/beverage/BeverageDescription.java
@@ -1,9 +1,36 @@
 package it.unipv.ingsw.d20.beverage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BeverageDescription {
 
+	String code;
+	double price;
+	private Map<Ingredient, Double> ingredients;  
+	
+	public BeverageDescription(String code,double price) {
+		this.code=code;
+		this.price=price;
+		this.ingredients=new HashMap<Ingredient, Double>();
+	}
+	//ATTENZIONE: serve ingredient? si riduce ad una stringa...
+	public void addIngredient(Ingredient i,double q) {
+		ingredients.put(i, q);
+	}
+	public void setIngredientQuantity(Ingredient i,double q){
+		//da fare
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public Map<Ingredient, Double> getIngredients() {
+		return ingredients;
+	}
+
 	public double getPrice() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
