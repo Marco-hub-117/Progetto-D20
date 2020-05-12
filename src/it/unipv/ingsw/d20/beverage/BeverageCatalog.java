420b7ca98a0273af10db066926d448989fd30e94
Enrico <luca.malovini01@universitadipavia.it>
2020-05-12 15:23:09 +0200

    modifica BeverageCatalog

--- src/it/unipv/ingsw/d20/beverage/BeverageCatalog.java
+++ src/it/unipv/ingsw/d20/beverage/BeverageCatalog.java
@@ -18,6 +18,7 @@ public class BeverageCatalog {
 	
 	public void addBeverageDescription(BeverageDescription b) {
 		//catalog.put(, )
+		int x;
 	}
 	public Map<String, BeverageDescription> getCatalog() {
 		return catalog;
