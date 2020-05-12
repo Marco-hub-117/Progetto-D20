10e68561106cb56e100a3b35a2a716376fe7dddd
davide <davide.spalenza01@universitadipavia.it>
2020-05-12 15:07:43 +0200

    Aggiunta Tank

--- src/it/unipv/ingsw/d20/beverage/Tank.java
+++ src/it/unipv/ingsw/d20/beverage/Tank.java
@@ -1,5 +1,23 @@
 package it.unipv.ingsw.d20.beverage;
 
 public class Tank {
-
+	
+	private String id;
+	private double level;
+	private double temperature;
+	//quando viene instanziato un serbatoio, è vuoto.
+	//L'id verrà assegnto dalla macchina cui appartiene il serbatoio
+	public Tank(String id) {
+		this.id=id;
+	}
+	public String getId() {
+		return id;
+	}
+	public double getLevel() {
+		return level;
+	}
+	public double getTemperature() {
+		return temperature;
+	}
+	
 }
