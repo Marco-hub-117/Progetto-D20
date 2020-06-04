ee5e3785e98890643e2b17c09f5f4c405145b2f9
davide <davide.spalenza01@universitadipavia.it>
2020-06-04 21:22:38 +0200

    cambiamenti minori

--- src/it/unipv/ingsw/d20/vendingmachine/gui/operator/OperatorGui.java
+++ src/it/unipv/ingsw/d20/vendingmachine/gui/operator/OperatorGui.java
@@ -8,13 +8,9 @@ public class OperatorGui extends JFrame {
 			
 			setSize(600,400);
 			setTitle("Interfaccia operatore");
-<<<<<<< HEAD
-			setLayout(new BorderLayout());
-			p=new Pannello(5);
-=======
+
 			//setLayout(new BorderLayout());
 			p=new Pannello(numeroTank);
->>>>>>> branch 'master' of https://github.com/IngSW-unipv/Progetto-D20.git
 			this.add(p);
 		}
 }
