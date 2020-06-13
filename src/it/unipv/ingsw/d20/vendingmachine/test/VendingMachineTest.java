4ff62efb21b6ca7bf4034eb6060ddf46066f170b
faSUro <fasulo.nicol@gmail.com>
2020-06-13 11:07:37 +0200

    added JUnit Test Case

---
+++ src/it/unipv/ingsw/d20/vendingmachine/test/VendingMachineTest.java
@@ -0,0 +1,18 @@
+package it.unipv.ingsw.d20.vendingmachine.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import it.unipv.ingsw.d20.vendingmachine.model.VendingMachine;
+
+class VendingMachineTest {
+	
+	private final VendingMachine vendingMachine = new VendingMachine("TEST_VM");
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
