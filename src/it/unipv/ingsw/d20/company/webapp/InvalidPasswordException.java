39ae1866d4032c43d564ee41e3da3d750ad8f17d
lzdp15 <luigidelpio15@mail.com>
2020-06-15 23:25:45 +0200

    WebApp changes

--- src/it/unipv/ingsw/d20/company/webapp/InvalidPasswordException.java
+++ src/it/unipv/ingsw/d20/company/webapp/InvalidPasswordException.java
@@ -3,4 +3,8 @@ package it.unipv.ingsw.d20.company.webapp;
 @SuppressWarnings("serial")
 public class InvalidPasswordException extends Exception {
 
+	public InvalidPasswordException() {
+		super("Invalid password entered");
+	}
+
 }
--- src/it/unipv/ingsw/d20/company/webapp/WebAppController.java
+++ src/it/unipv/ingsw/d20/company/webapp/WebAppController.java
@@ -1,10 +1,35 @@
 package it.unipv.ingsw.d20.company.webapp;
 
+import java.util.List;
+
 public class WebAppController {
 	
-	@SuppressWarnings("unused")
-	private boolean logged;
+	//IN FASE DI ELABORAZIONE
 	
-	//renderla singleton
+	
+	//le classi "Vending" ecc sono solo indicative
+	
+	//public List<Vending> getAllVendingMachines() {};
+	//public Vending getVendingMachine(String id) {};
+	//public String getVendingMachineInfo(String id) {}; //location, type, status
+	//public Double[] getTankLevels(String id);
+	//public Double[] getTankTemps(String id);
+	//public void setTankTemps(String id, Double[] temps);
+	//public Double[] getCurrentAmount(String id);
+	
+	
+	//public List<Operator> getAllOperators() {};
+	//public Operator getOperator(String username) {};
+	public void addOperator(String name, String username, String password, String type) {};
+	
+	
+	public void getAllKeys() {};
+	public void addKey() {};
+	public void activateKey() {};
+	public void deactivateKey() {};
+	
+	
+	public void getAllBeverageDescriptions() {};
+	public void setBeverageDescription() {};
 
 }
