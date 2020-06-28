fdaa6c0e81fa2d4e88ff85ffa9cbe15587ff802a
faSUro <fasulo.nicol@gmail.com>
2020-06-28 22:01:51 +0200

    added malfunction generator

---
+++ src/it/unipv/ingsw/d20/vendingmachine/MalfunctionGeneratorFrame.java
@@ -0,0 +1,34 @@
+package it.unipv.ingsw.d20.vendingmachine;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class MalfunctionGeneratorFrame extends JFrame {
+	
+	private static final int WIDTH = 200, HEIGHT = 150;
+	
+	public MalfunctionGeneratorFrame(String IDNumber) {
+		setTitle("Abort " + IDNumber);
+		setSize(WIDTH, HEIGHT);
+		
+		Container container = getContentPane();
+		JPanel panel = new JPanel();
+		container.add(panel);
+		panel.setLayout(new BorderLayout());
+		
+		JButton abortBtn = new JButton("Abort");
+		abortBtn.setFont(abortBtn.getFont().deriveFont(Font.BOLD, 42));
+		abortBtn.addActionListener(al -> { System.exit(-1); });
+		panel.add(abortBtn, BorderLayout.CENTER);
+		
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
