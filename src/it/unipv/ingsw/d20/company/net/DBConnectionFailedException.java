5d30c723cf1d61025403cb7ba00810db31aa1512
faSUro <fasulo.nicol@gmail.com>
2020-06-29 16:46:56 +0200

    updated client-server function (to be fixed)

--- src/it/unipv/ingsw/d20/company/net/DBConnectionFailedException.java
+++
@@ -1,9 +0,0 @@
-package it.unipv.ingsw.d20.company.net;
-
-@SuppressWarnings("serial")
-public class DBConnectionFailedException extends Exception{
-	
-	public DBConnectionFailedException() {
-		super("Si è verificato un problema di connesione con il database. Controllare che il database Server sia attivo");
-	}
-}
